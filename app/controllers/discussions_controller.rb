d66215c9538a9cee03a050e749f992858286bec6
Gregory Brown <gregory.t.brown@gmail.com>
2012-03-17 20:45:50 +0000

    horribly rough toggling of open/closed status

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,15 +2,28 @@ class DiscussionsController < ApplicationController
   before_filter :find_course
 
   def index
-    @discussions = case params[:category]
-    when "conversation"
-      @course.discussions.active.conversations
-    when "review"
-      @course.discussions.active.reviews
-    when "evaluation"
-      @course.discussions.active.evaluations
+    if params[:state] == "closed"
+      @discussions = case params[:category]
+      when "conversation"
+        @course.discussions.inactive.conversations
+      when "review"
+        @course.discussions.inactive.reviews
+      when "evaluation"
+        @course.discussions.inactive.evaluations
+      else
+        @course.discussions.inactive.conversations
+      end
     else
-      @course.discussions.active.conversations
+      @discussions = case params[:category]
+      when "conversation"
+        @course.discussions.active.conversations
+      when "review"
+        @course.discussions.active.reviews
+      when "evaluation"
+        @course.discussions.active.evaluations
+      else
+        @course.discussions.active.conversations
+      end
     end
   end
 
