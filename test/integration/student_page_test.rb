9cb0545481cc308deb761f3d70fb051904796b20
Chris Geihsler <chris@geihsler.net>
2012-03-25 13:09:53 -0400

    added failing spec for instructors seeing a task completion link

--- test/integration/student_page_test.rb
+++ test/integration/student_page_test.rb
@@ -5,22 +5,37 @@ describe "Student Page Integration" do
   before do
     @course     = FactoryGirl.create(:webdev)
     @student    = build_person(:student, @course)
+    @instructor = build_person(:instructor, @course)
+  end
 
+  it "should display a read-only status for each course task" do
     sign_in @student
 
     click_link "Web Development"
     click_link "Student"
-  end
 
-  it "should display all of the course tasks on the student page" do
     @course.tasks.each do |t|
       within_task(t) do 
         assert_task_is_on_page(t)
         assert_task_is_incomplete(t)
+        assert_complete_task_link_does_not_exist
       end
     end
   end
 
+  it "should allow an instructor to complete a task" do
+    sign_in @instructor
+
+    click_link "Web Development"
+    click_link "Student"
+
+    task = @course.tasks.first
+
+    within_task(task) do
+      has_link?("Mark as complete").must_equal true, "The link to complete the task does not exist"
+    end
+    
+  end
 
 end
 
@@ -30,6 +45,14 @@ def within_task(task)
   within(:xpath, task_xpath(task)) do
     yield
   end
+end
+
+def assert_complete_task_link_exists
+  has_link?("Mark as complete").must_equal true, "The link to complete the task does not exist"
+end
+
+def assert_complete_task_link_does_not_exist
+  has_link?("Mark as complete").must_equal false, "The link to complete the task should not exist"
 end
 
 def assert_task_is_incomplete(task)
