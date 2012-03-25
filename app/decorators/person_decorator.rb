b55c42d6b7df79996a03f197dcc2c0392bed9f3a
Gregory Brown <gregory.t.brown@gmail.com>
2012-03-25 19:41:31 -0400

    add simple gravatar functionality

--- app/decorators/person_decorator.rb
+++ app/decorators/person_decorator.rb
@@ -1,7 +1,7 @@
 class PersonDecorator < ApplicationDecorator
   decorates :person, :class => Clubhouse::Client::Person
 
-  allows :name, :email, :github_nickname, :permissions
+  allows :name, :email, :github_nickname, :permissions, :gravatar_url
 
   def has_role?(role, course)
     membership = course.course_memberships.for_person(person).first
