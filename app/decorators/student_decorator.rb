2a45108bbf467d1c77f85613c5971ced16b4b18d
Chris Geihsler <chris@geihsler.net>
2012-03-25 12:17:00 -0400

    refactored student page integration spec to use data attribs

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -2,7 +2,7 @@ class StudentDecorator < ApplicationDecorator
   decorates :course_membership
 
   def tasks
-    course_membership.course.tasks.map(&:description)
+    course_membership.course.tasks
   end
 
 end
--- test/integration/student_page_test.rb
+++ test/integration/student_page_test.rb
@@ -9,11 +9,23 @@ describe "Student Page Integration" do
 
   it "should display all of the course tasks on the student page" do
     sign_in @student
+
     click_link "Web Development"
     click_link "Student"
 
-    page.body.must_include "Community Service"
-    page.body.must_include "Personal Project"
-    page.body.must_include "Challenge"
+    @course.tasks.each do |t|
+      assert_task_is_on_page page, t
+    end
   end
+
+end
+
+private
+
+def assert_task_is_on_page(page, task)
+  includes_task_row?(page, task).must_equal true, "Could not find task '#{task.description}' on the page"
+end
+
+def includes_task_row?(page, task)
+  page.has_xpath?("//tr[@data-taskid='#{task.id}']")
 end
