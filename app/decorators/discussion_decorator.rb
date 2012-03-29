bd40b02004c27192ebc16d29fed6f25784cc07fb
Gregory Brown <gregory.t.brown@gmail.com>
2012-03-29 18:50:13 -0400

    Minor refactorings

--- app/decorators/discussion_decorator.rb
+++ app/decorators/discussion_decorator.rb
@@ -12,22 +12,40 @@ class DiscussionDecorator < ApplicationDecorator
   
   def header
     h.content_tag(:p, start_date, :class => ["discussions","date"]) +
-    h.image_tag(author.gravatar_url(30), :class => "discussions") +
+    h.image_tag(author.gravatar_url(30), :class => "discussions")   +
     h.content_tag(:h2, discussion.subject)
   end
   
   def body
     discussion.body.to_s.html_safe
   end
+
+  def state
+    discussion.archived ? "closed" : "open"
+  end
   
   # TODO: Support unarchive
   def footer
-    link_params = [h.params["course_id"], h.params["id"]]
-    [ h.link_to("Edit", h.edit_course_discussion_path(*link_params)), 
-      h.link_to_if(!discussion.archived, "Archive", 
-                h.course_discussion_path(*link_params), 
-                :method => :delete, :confirm => "Are you sure?") { "Unarchive" },
-      h.link_to("Back to #{discussion.category}", h.course_discussions_path(:category => discussion.category))
-     ].join(" | ").html_safe
+    [ edit_link, archive_link, discussion_list_link ].join(" | ").html_safe
+  end
+
+  private
+
+  def edit_link
+    path = h.edit_course_discussion_path(discussion.course.id, discussion.id)
+
+    h.link_to("Edit", path)
+  end
+
+  def archive_link
+    h.link_to_if(!discussion.archived, "Archive", 
+      h.course_discussion_path(discussion.course.id, discussion.id), 
+      :method => :delete, :confirm => "Are you sure?") { "Unarchive" }
+  end
+
+  def discussion_list_link
+    h.link_to("Back to #{discussion.category}", 
+      h.course_discussions_path(:category => discussion.category, 
+                                :state    => state))
   end
 end
