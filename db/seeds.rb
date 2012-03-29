7f55898380beccf827d4858cf82a02fb321b3ef0
Gregory Brown <gregory.t.brown@gmail.com>
2012-03-29 18:56:36 -0400

    Remove conversation seeds, no longer needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,33 +11,3 @@ people = [frank, barbora]
 people.each do |person|
   person.create(course_id: course.id, role: "Student") if person.empty?
 end
-
-conversations = [ "How awesome is Jordan Byron?",
-                  "How many unicorns does it take to make a rainbow?",
-                  "Can someone ask a serious question please?" ]
-
-conversations.each do |e|
-  course.discussions.find_or_create_by_subject(
-    :subject => e, :category => "conversations"
-  )
-end
-
-evaluations =  [ "My s10-e2 is ready for evaluation",
-                 "Revisions have been made to my s10-e1",
-                 "Come on, evaluate it, you know you want to!" ]
-
-evaluations.each do |e|
-  course.discussions.find_or_create_by_subject(
-    :subject => e, :category => "evaluations"
-  )
-end
-
-reviews = ["Messy first spike on my individual project",
-          "Proof of concept patch to add archives to Newman",
-          "If you don't review this soon, I might go crazy!"]
-
-reviews.each do |e|
-  course.discussions.find_or_create_by_subject(
-    :subject => e, :category => "reviews"
-  )
-end
