420c24299fe704cc6b515607d7cc77770e2ed232
Vinicius Horewicz <vinicius@horewi.cz>
2012-04-01 16:17:07 -0300

    Update study plan

--- app/controllers/study_plans_controller.rb
+++ app/controllers/study_plans_controller.rb
@@ -2,6 +2,17 @@ class StudyPlansController < ApplicationController
   before_filter :find_course, :find_student
 
   def show
+    @study_plan = StudyPlanDecorator.new(@student.study_plan)
+  end
+
+  def edit
     @study_plan = @student.study_plan
+  end
+
+  def update
+    @student.study_plan.update_attribute(:content, params[:study_plan][:content])
+    flash[:notice] = "Study plan updated."
+
+    redirect_to(course_student_plan_path(@course, @student))
   end
 end
--- test/functional/study_plans_controller_test.rb
+++ test/functional/study_plans_controller_test.rb
@@ -7,8 +7,23 @@ describe StudyPlansController do
     @controller.current_person = @student
   end
 
+  def study_plan_params(params = {})
+    { course_id: @course.id,
+      student_id: @student.github_nickname
+    }.merge(params)
+  end
+
   it "automatically creates study plan on #show" do
-    get(:show, course_id: @course.id, student_id: @student.github_nickname)
+    get(:show, study_plan_params)
     assigns(:study_plan).wont_equal nil
+  end
+
+  it "updates study plan" do
+    put(:update, study_plan_params(study_plan: { content: "New plan" }))
+    plan = StudyPlan.last
+
+    plan.content.must_equal "New plan"
+    flash[:notice].must_equal "Study plan updated."
+    response.redirect?.must_equal true
   end
 end
